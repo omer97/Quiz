1c787ef982862289f379cf6185afccff2f25057c
DESKTOP-HFGNDAP\CBM <omerakber97@gmail.com>
2023-01-11 10:15:20 +0500

    Added Copy to clipboard button locator. Pop up implementation not done yet

--- Quiz/UnitTest1.cs
+++ Quiz/UnitTest1.cs
@@ -37,19 +37,14 @@ namespace Quiz
             leftpane.SendKeys("The clickAndWait command doesn't get converted when you choose the Webdriver format in the Selenium IDE. Here is the workaround. Add the wait line below. Realistically, the problem was the click or event that happened before this one—line 1 in my C# code. But really, just make sure you have a WaitForElement before any action where you're referencing a \"By\" object.");
 
             IWebElement btnCreateQuiz = driver.FindElement(By.Id(":r1:"));
-            btnCreateQuiz.Click();
-
-
+            btnCreateQuiz.Click(); 
             //new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(driver =>
             // driver.FindElement(By.Id("scrollable-quiz-content-box")).FindElements(By.ClassName("MuiBox-root")).ToList().Count > 0
             //);
-
-
+             
             //  new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(driver =>
             // driver.FindElement(By.XPath("//*[@id=\"root\"]/div[1]/div[1]/div/div/div[1]/div/form/div[2]/div[1]/button[2]"))
-            //);
-
-
+            //); 
             //driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(100);
 
             Thread.Sleep(10000);
@@ -59,11 +54,17 @@ namespace Quiz
             IWebElement rightPane = driver.FindElement(By.Id("scrollable-quiz-content-box"));
             List<IWebElement> questionAnswerListChildren = rightPane.FindElements(By.XPath("//div[contains(@id,'Question-Answer-box')]")).ToList<IWebElement>();
 
-           
+
             foreach (var q in questionAnswerListChildren)
             {
                 var questionList = q.FindElements(By.XPath("*"));
 
+                //checkbox code
+                questionList[0].FindElement(By.ClassName("PrivateSwitchBase-input")).Click(); 
+                //checkbox code end
+
+
+               
 
                 IWebElement question = questionList[0].FindElement(By.TagName("textarea"));
                 string questionText = question.Text;
@@ -78,27 +79,38 @@ namespace Quiz
                     IWebElement answer = ans.FindElement(By.TagName("textarea"));
                     string answerText = answer.Text;
                     ansList.Add(answerText);
-                   // if (String.IsNullOrEmpty(answerText)) { break; }
-                }
-
-
+                    // if (String.IsNullOrEmpty(answerText)) { break; }
+                } 
             }
-
-
+             
             if (qustList.Count > 0 && ansList.Count > 0)
             {
-               // Assert.Pass();
+                // Assert.Pass();
             }
             else
             {
                 //Assert.Fail();
-               
-            }
 
-
-
+            } 
 
         }
+
+        [Test,Order(3)]
+        public void CopyToClipBoard()
+        {
+            IWebElement rightPane = driver.FindElement(By.ClassName("css-ier0s8"));
+            rightPane.FindElements(By.TagName("div"))[0].FindElement(By.TagName("button")).Click();
+
+            Thread.Sleep(5000);
+        }
+
+        public void SelectQuestions()
+        {
+
+        }
+
+
+
         [OneTimeTearDown]
         public void Exit()
         {
